e1fd27e4e20b4776f79a1dfb1fba0984f3d710f4
sillyroedeerpig <13581452+sillyroedeerpig@user.noreply.gitee.com>
2024-06-14 18:50:47 +0800

    audit manage

--- src/views/NewsSandBox/Audit/index.jsx
+++ src/views/NewsSandBox/Audit/index.jsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Table, Button, Tooltip, notification } from 'antd';
+import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
 export default function Audit() {
+    const [dataSource, setDataSource] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const { roleId, username, region } = JSON.parse(localStorage.getItem('token'));
+    useEffect(() => {
+        setLoading(true);
+        const roleObj = {
+            "1": "superadmin",
+            "2": "admin",
+            "3": "editor"
+        }
+        axios.get(`/news?auditState=1&_expand=category`).then(res => {
+            setDataSource(roleObj[roleId] === 'superadmin' ? res.data : [
+                ...res.data.filter(item => item.author === username),
+                ...res.data.filter(item => item.region === region && roleObj[item.roleId] === "editor")
+            ]);
+            setLoading(false);
+        })
+    }, [region, roleId, username]);
+
+    // Table组件相关
+    const columns = [
+        {
+            title: "新闻标题",
+            dataIndex: "label",
+            render: (label, item) => {
+                return <Link to={`/news-manage/preview/${item.id}`}>{label}</Link>
+            }
+        },
+        {
+            title: "作者",
+            dataIndex: "author"
+        },
+        {
+            title: "新闻分类",
+            dataIndex: "category",
+            render: (category) => {
+                return category.label
+            }
+        },
+        {
+            title: "操作",
+            render: (item) => {
+                return (
+                    <div className='operate-button'>
+                        <Tooltip title='不通过'>
+                            <Button danger type="primary" shape="circle" icon={<CloseOutlined />} onClick={() => handleAuditNews(item.id, 3, 0)} disabled={item.default}></Button>
+                        </Tooltip>
+                        <Tooltip title='通过'>
+                            <Button type="primary" shape="circle" icon={<CheckOutlined />} disabled={item.default} onClick={() => handleAuditNews(item.id, 2, 1)}></Button>
+                        </Tooltip>
+                    </div >
+                )
+            }
+        },
+    ];
+
+    // passing & fail button
+    const handleAuditNews = (id, auditState, publishState) => {
+        setDataSource(dataSource.filter(item => item.id !== id));
+        axios.patch(`/news/${id}`, {
+            auditState,
+            publishState
+        }).then(()=>{
+            notification.info({
+                message: `通知`,
+                description:
+                    `您可以到到【审核管理/审核列表】中查看您的新闻的审核状态`,
+                placement: "bottomRight",
+            });
+        })
+    }
+
     return (
-        <div>Audit</div>
+        <div>
+            {/* Table相关 */}
+            <Table dataSource={dataSource} columns={columns} pagination={{ pageSize: 5 }} loading={loading} rowKey={item => item.id}></Table>
+        </div>
     )
 }
--- src/views/NewsSandBox/AuditList/index.jsx
+++ src/views/NewsSandBox/AuditList/index.jsx
@@ -1,7 +1,105 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Table, Tag, notification } from 'antd';
+import axios from 'axios';
 
 export default function AuditList() {
+    const [dataSource, setDataSource] = useState([]);
+    const navigate = useNavigate();
+    // 取本地数据
+    const { username } = JSON.parse(localStorage.getItem('token'));
+    // 取表数据
+    useEffect(() => {
+        axios.get(`/news?author=${username}&auditState_ne=0&publishState_lte=1&_expand=category`).then(res => {
+            setDataSource(res.data);
+        })
+    }, [username]);
+
+
+    const columns = [
+        {
+            title: "新闻标题",
+            dataIndex: "label",
+            render: (label, item) => {
+                return <Link to={`/news-manage/preview/${item.id}`}>{label}</Link>
+            }
+        },
+        {
+            title: "作者",
+            dataIndex: "author"
+        },
+        {
+            title: "新闻分类",
+            dataIndex: "category",
+            render: (category) => {
+                return category.label
+            }
+        },
+        {
+            title: "审核状态",
+            dataIndex: "auditState",
+            render: (auditState) => {
+                const colorList = ['default', 'warning', 'success', 'error'];
+                const auditList = ['', '审核中', '已通过', '未通过'];
+
+                return <Tag color={`${colorList[auditState]}`}>{auditList[auditState]}</Tag>
+            }
+        },
+        {
+            title: "操作",
+            render: (item) => {
+                return (
+                    <div className='operate-button'>
+                        {
+                            item.auditState === 1 && <Button danger onClick={() => handleRevert(item.id)}>撤销</Button>
+                        }
+                        {
+                            item.auditState === 2 && <Button type="primary" onClick={() => handlePublish(item.id)}>发布</Button>
+                        }
+                        {
+                            item.auditState === 3 && <Button onClick={() => handleUpdate(item.id)}>修改</Button>
+                        }
+                    </div >
+                )
+            }
+        }
+    ];
+
+    // 撤销按钮
+    const handleRevert = (id) => {
+        axios.patch(`/news/${id}`, {
+            auditState: 0
+        }).then(() => {
+            setDataSource(dataSource.filter(item => item.id !== id));
+            notification.info({
+                message: `通知`,
+                description:
+                    `您可以到草稿箱中查看您的新闻`,
+                placement: "bottomRight",
+            });
+        })
+    }
+    // 发布按钮
+    const handlePublish = (id) => {
+        axios.patch(`/news/${id}`, {
+            publishState: 2,
+            publishTime: Date.now(),
+        }).then(() => {
+            navigate(`/publish-manage/published`);
+            notification.info({
+                message: `通知`,
+                description:
+                    `您可以到【发布管理/已发布】中查看您的新闻`,
+                placement: "bottomRight",
+            });
+        })
+    }
+    // 修改按钮
+    const handleUpdate = (id) => {
+        navigate(`/news-manage/update/${id}`);
+    }
+
     return (
-        <div>AuditList</div>
+        <Table dataSource={dataSource} columns={columns} rowKey={(item) => item.id} pagination={{ pageSize: 5 }}></Table>
     )
 }
